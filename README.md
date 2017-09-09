e89cb5310561be471f2930f2fe4971ad0d531f34
nagasudhirpulla <nagasudhirpulla@gmail.com>
2017-09-09 11:17:02 +0530

    data display hover event added

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# schedule_monitor
+# Schedule Monitor
 Monitors the schedules of generators
 
 ![Grid Assistant UI Snapshot](https://github.com/POSOCO/schedule_monitor/raw/master/screenshot_1.png)
+
+## Links
+1. Overwrite local changes with remote branch - https://stackoverflow.com/questions/1125968/how-do-i-force-git-pull-to-overwrite-local-files
--- views/reserve.js
+++ views/reserve.js
@@ -1,13 +1,13 @@
-var getHeadingsList = function() {
-  var headingsList = [];
-  for (var i = 0; i < reservesArray_.length; i++) {
-    headingsList.push(reservesArray_[i][0]);
-  }
-  return headingsList;
+var getHeadingsList = function () {
+    var headingsList = [];
+    for (var i = 0; i < reservesArray_.length; i++) {
+        headingsList.push(reservesArray_[i][0]);
+    }
+    return headingsList;
 };
 
-var getBlockValue = function(blk, arrIndex) {
-  return reservesArray_[arrIndex][blk];
+var getBlockValue = function (blk, arrIndex) {
+    return reservesArray_[arrIndex][blk];
 };
 
 var headingsArray;
@@ -25,162 +25,194 @@ var pageRefreshTimerId_ = null;
 window.onload = doOnLoadStuff();
 
 function doOnLoadStuff() {
-  doPageRefreshSetup();
-  headingsArray = getHeadingsList();
-  findDuplicates(headingsArray);
-  /*
-   var onBarHeadings = findOnBarSchHeadings(headingsArray);
-   console.log(onBarHeadings['onBarHeadings'].join(","));
-   console.log(onBarHeadings['onBarHeadings'].length + "\n\n");
-   console.log(onBarHeadings['schHeadings'].join(","));
-   console.log(onBarHeadings['schHeadings'].length + "\n\n");
-   console.log(onBarHeadings['generatorNames'].join(","));
-   console.log(onBarHeadings['generatorNames'].length + "\n\n");
-   */
-  for (var i = 0; i < onBarHeadings.length; i++) {
-    onBarHeadingsIndices[i] = headingsArray.indexOf(onBarHeadings[i]);
-  }
-  for (var i = 0; i < schHeadings.length; i++) {
-    schHeadingsIndices[i] = headingsArray.indexOf(schHeadings[i]);
-  }
-  revNumber = reservesArray_[headingsArray.indexOf("REVNO_MAX")][1];
-  stacksDiv = document.getElementById("reserveStacksDiv");
-  traceReservesPlot();
-};
+    doPageRefreshSetup();
+    headingsArray = getHeadingsList();
+    findDuplicates(headingsArray);
+    /*
+     var onBarHeadings = findOnBarSchHeadings(headingsArray);
+     console.log(onBarHeadings['onBarHeadings'].join(","));
+     console.log(onBarHeadings['onBarHeadings'].length + "\n\n");
+     console.log(onBarHeadings['schHeadings'].join(","));
+     console.log(onBarHeadings['schHeadings'].length + "\n\n");
+     console.log(onBarHeadings['generatorNames'].join(","));
+     console.log(onBarHeadings['generatorNames'].length + "\n\n");
+     */
+    for (var i = 0; i < onBarHeadings.length; i++) {
+        onBarHeadingsIndices[i] = headingsArray.indexOf(onBarHeadings[i]);
+    }
+    for (var i = 0; i < schHeadings.length; i++) {
+        schHeadingsIndices[i] = headingsArray.indexOf(schHeadings[i]);
+    }
+    revNumber = reservesArray_[headingsArray.indexOf("REVNO_MAX")][1];
+    stacksDiv = document.getElementById("reserveStacksDiv");
+    traceReservesPlot();
+}
 
 function traceReservesPlot() {
-  var genSelectionElements = document.getElementsByClassName("gen_select");
-  activeGenerators = [];
-  for (var i = 0; i < genSelectionElements.length; i++) {
-    var isCheckedStatus = genSelectionElements[i].checked;
-    if (typeof isCheckedStatus != "undefined" && isCheckedStatus == true) {
-      activeGenerators.push(genSelectionElements[i].value);
+    var genSelectionElements = document.getElementsByClassName("gen_select");
+    activeGenerators = [];
+    for (var i = 0; i < genSelectionElements.length; i++) {
+        var isCheckedStatus = genSelectionElements[i].checked;
+        if (typeof isCheckedStatus != "undefined" && isCheckedStatus == true) {
+            activeGenerators.push(genSelectionElements[i].value);
+        }
     }
-  }
-  var xLabels = [];
-  for (var i = 0; i < 96; i++) {
-    xLabels.push(i + 1);
-  }
-  var traces = [];
-  for (var i = 0; i < generatorNames.length; i++) {
-    if (activeGenerators.indexOf(generatorNames[i]) == -1) {
-      continue;
+    var xLabels = [];
+    for (var i = 0; i < 96; i++) {
+        xLabels.push(i + 1);
     }
-    var yLabels = [];
-    var textTooltips = [];
-    for (var k = 0; k < 96; k++) {
-      var value = getBlockValue(k + 1, onBarHeadingsIndices[i]) - getBlockValue(k + 1, schHeadingsIndices[i]);
-      if (value < 0) {
-        value = 0;
-      }
-      yLabels[k] = value;
-      textTooltips[k] = generatorNames[i] + " (" + (k + 1) + ")";
+    var traces = [];
+    for (var i = 0; i < generatorNames.length; i++) {
+        if (activeGenerators.indexOf(generatorNames[i]) == -1) {
+            continue;
+        }
+        var yLabels = [];
+        var textTooltips = [];
+        for (var k = 0; k < 96; k++) {
+            var value = getBlockValue(k + 1, onBarHeadingsIndices[i]) - getBlockValue(k + 1, schHeadingsIndices[i]);
+            if (value < 0) {
+                value = 0;
+            }
+            yLabels[k] = value;
+            textTooltips[k] = generatorNames[i] + " (" + (k + 1) + ")";
+        }
+        traces.push({
+            x: xLabels,
+            y: yLabels,
+            fill: 'tonexty',
+            name: generatorNames[i]
+        });
     }
-    traces.push({
-      x: xLabels,
-      y: yLabels,
-      fill: 'tonexty',
-      name: generatorNames[i]
-    });
-  }
-  traces[0].fill = 'tozeroy';
-  var layout = {
-    title: 'Spinning Reserve Margin Status as per Revision number ' + revNumber,
-    xaxis: {
-      title: 'Block Number',
-      dtick: 2
-    },
-    yaxis: {
-      title: 'Margins'
-    },
-    hovermode: 'closest'
-  };
+    traces[0].fill = 'tozeroy';
+    var layout = {
+        title: 'Spinning Reserve Margin Status as per Revision number ' + revNumber,
+        xaxis: {
+            title: 'Block Number',
+            dtick: 2
+        },
+        yaxis: {
+            title: 'Margins'
+        },
+        hovermode: 'closest'
+    };
 
-  Plotly.newPlot(stacksDiv, stackedArea(traces), layout);
+    Plotly.newPlot(stacksDiv, stackedArea(traces), layout);
+
+    stacksDiv
+        .on('plotly_hover', function (data) {
+            if (data.points.length > 0) {
+                var pointIndex = data.points[0]['pointNumber'];
+                var textDataArray = document.getElementById("reserveStacksDiv").data;
+                var infoStrings = [];
+                for (var i = textDataArray.length - 1; i >= 0; i--) {
+                    infoStrings.push(textDataArray[i]['text'][pointIndex]);
+                }
+                document.getElementById("reserveInfoDiv").innerHTML = "BLOCK (" + data.points[0]['x'] + ')<div style="height: 5px"></div>' + infoStrings.join('<div style="height: 5px"></div>');
+            }
+        })
+        .on('plotly_unhover', function (data) {
+            //document.getElementById("reserveInfoDiv").innerHTML = '';
+        });
 }
 
 function refreshReservePlot() {
-  traceReservesPlot();
+    traceReservesPlot();
 }
 
 function findDuplicates(arr) {
-  var sorted_arr = arr.slice().sort(); // You can define the comparing function here.
-  var results = [];
-  for (var i = 0; i < arr.length - 1; i++) {
-    if (sorted_arr[i + 1] == sorted_arr[i]) {
-      results.push(sorted_arr[i]);
+    var sorted_arr = arr.slice().sort(); // You can define the comparing function here.
+    var results = [];
+    for (var i = 0; i < arr.length - 1; i++) {
+        if (sorted_arr[i + 1] == sorted_arr[i]) {
+            results.push(sorted_arr[i]);
+        }
     }
-  }
-  if (results.length > 0) {
-    console.log("Duplicate headings are " + results);
-  }
+    if (results.length > 0) {
+        console.log("Duplicate headings are " + results);
+    }
 }
 
 function stackedArea(traces) {
-  var i, j;
-  for (i = 0; i < traces.length; i++) {
-    traces[i].text = [];
-    traces[i].hoverinfo = 'text';
-    for (j = 0; j < (traces[i]['y'].length); j++) {
-      traces[i].text.push(traces[i]['name'] + " (" + traces[i]['y'][j].toFixed(0) + ")");
+    var i, j;
+    for (i = 0; i < traces.length; i++) {
+        traces[i].text = [];
+        traces[i].hoverinfo = 'text';
+        for (j = 0; j < (traces[i]['y'].length); j++) {
+            traces[i].text.push(traces[i]['name'] + " (" + traces[i]['y'][j].toFixed(0) + ")");
+        }
     }
-  }
-  for (i = 1; i < traces.length; i++) {
-    for (j = 0; j < (Math.min(traces[i]['y'].length, traces[i - 1]['y'].length)); j++) {
-      traces[i]['y'][j] += traces[i - 1]['y'][j];
+    for (i = 1; i < traces.length; i++) {
+        for (j = 0; j < (Math.min(traces[i]['y'].length, traces[i - 1]['y'].length)); j++) {
+            traces[i]['y'][j] += traces[i - 1]['y'][j];
+        }
     }
-  }
-  return traces;
+    return traces;
 }
 
 function findOnBarSchHeadings(arr) {
-  var onBarHeadings = [];
-  var onBarHeadingsIndices = [];
-  var schHeadings = [];
-  var schHeadingsIndices = [];
-  var generatorNames = [];
+    var onBarHeadings = [];
+    var onBarHeadingsIndices = [];
+    var schHeadings = [];
+    var schHeadingsIndices = [];
+    var generatorNames = [];
 
-  for (var i = 0; i < arr.length - 1; i++) {
-    if (/_DC_ONBAR$/.test(arr[i])) {
-      onBarHeadings.push(arr[i]);
-      onBarHeadingsIndices.push(i);
-      generatorNames.push(arr[i].substr(0, arr[i].length - "_DC_ONBAR".length));
+    for (var i = 0; i < arr.length - 1; i++) {
+        if (/_DC_ONBAR$/.test(arr[i])) {
+            onBarHeadings.push(arr[i]);
+            onBarHeadingsIndices.push(i);
+            generatorNames.push(arr[i].substr(0, arr[i].length - "_DC_ONBAR".length));
+        }
     }
-  }
 
-  for (var i = 0; i < generatorNames.length; i++) {
-    var generatorName = generatorNames[i];
-    for (var k = 0; k < arr.length - 1; k++) {
-      if (generatorName + "_TOTAL" == arr[k]) {
-        schHeadings.push(arr[k]);
-        schHeadingsIndices.push(k);
-        break;
-      }
+    for (var i = 0; i < generatorNames.length; i++) {
+        var generatorName = generatorNames[i];
+        for (var k = 0; k < arr.length - 1; k++) {
+            if (generatorName + "_TOTAL" == arr[k]) {
+                schHeadings.push(arr[k]);
+                schHeadingsIndices.push(k);
+                break;
+            }
+        }
     }
-  }
 
-  return {
-    onBarHeadings: onBarHeadings,
-    schHeadings: schHeadings,
-    onBarHeadingsIndices: onBarHeadingsIndices,
-    schHeadingsIndices: schHeadingsIndices,
-    generatorNames: generatorNames
-  };
+    return {
+        onBarHeadings: onBarHeadings,
+        schHeadings: schHeadings,
+        onBarHeadingsIndices: onBarHeadingsIndices,
+        schHeadingsIndices: schHeadingsIndices,
+        generatorNames: generatorNames
+    };
 }
 
 function updateSchedulesNow() {
-  $.get("/tasks/schedules_fetch", function(data) {
-    location.reload();
-  });
+    $.get("/tasks/schedules_fetch", function (data) {
+        location.reload();
+    });
 }
 
 function doPageRefreshSetup() {
-  pageRefreshTimerId_ = setInterval(doPageRefresh, 120000);
+    pageRefreshTimerId_ = setInterval(doPageRefresh, 120000);
 }
 
 function doPageRefresh() {
-  var refreshCheckbtn = document.getElementById("togglePageRefreshBtn");
-  if (refreshCheckbtn.checked == true) {
-    location.reload();
-  }
+    var refreshCheckBtn = document.getElementById("togglePageRefreshBtn");
+    if (refreshCheckBtn.checked == true) {
+        location.reload();
+    }
+}
+
+function selectAllGen() {
+    var genSelectionElements = document.getElementsByClassName("gen_select");
+    for (var i = 0; i < genSelectionElements.length; i++) {
+        genSelectionElements[i].checked = true;
+    }
+    traceReservesPlot();
+}
+
+function deselectAllGen() {
+    var genSelectionElements = document.getElementsByClassName("gen_select");
+    for (var i = 0; i < genSelectionElements.length; i++) {
+        genSelectionElements[i].checked = false;
+    }
+    traceReservesPlot();
 }
